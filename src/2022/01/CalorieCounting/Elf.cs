0b0f3ccaa12b01ce6c4acaa0f24499bf1ed3d0cd
matjanko <mateusz.jankowski@prokonto.pl>
2022-12-01 10:25:08 +0100

    2022_01

---
+++ src/2022/01/CalorieCounting/Elf.cs
@@ -0,0 +1,14 @@
+﻿namespace CalorieCounting;
+
+public class Elf
+{
+    private readonly List<int> _calories = new();
+
+    public void AddCalories(int calories)
+    {
+        _calories.Add(calories);
+    }
+
+    public int GetTotalCalories() 
+        => _calories.Sum();
+}
