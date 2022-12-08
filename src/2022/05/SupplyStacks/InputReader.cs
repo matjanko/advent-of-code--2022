154474c08f5984404a3f37c4b0aa28321ce7dce3
matjanko <mateusz.jankowski@prokonto.pl>
2022-12-08 18:46:41 +0100

    2022_05

--- src/2022/05/SupplyStacks/InputReader.cs
+++ src/2022/05/SupplyStacks/InputReader.cs
@@ -11,22 +11,32 @@ public class InputReader
 
     public IEnumerable<Stack> ReadStacks()
     {
-        return null;
+        var inputs = _inputs.Where(x => x.Contains('[') && x.Contains(']')).ToList();
+        var stackNo = inputs.Select(x => x.Split("] [")).Max(x => x.Length);
+        var position = 1;
+        for (var i = 0; i < stackNo; i++)
+        {
+            var id = i + 1;
+            var crates = inputs.Select(input => input[position]).Where(crate => crate != ' ').ToList();
+            position += 4;
+            yield return new Stack(id, crates);
+        }
     }
     
     public IEnumerable<CraneMove> ReadCraneMoves()
     {
-        int crates, from, to;
-        var separator = " ";
+        const string separator = " ";
         foreach (var input in _inputs)
         {
-            if (input.StartsWith("move"))
+            if (!input.StartsWith("move"))
             {
-                crates = int.Parse(input.Split(separator)[1]);
-                from = int.Parse(input.Split(separator)[3]);
-                to = int.Parse(input.Split(separator)[5]);
-                yield return new CraneMove(crates, from, to);
+                continue;
             }
+            
+            var crates = int.Parse(input.Split(separator)[1]);
+            var from = int.Parse(input.Split(separator)[3]);
+            var to = int.Parse(input.Split(separator)[5]);
+            yield return new CraneMove(crates, from, to);
         }
     }
 }
--- src/2022/05/SupplyStacks/Stack.cs
+++ src/2022/05/SupplyStacks/Stack.cs
@@ -2,14 +2,33 @@ namespace SupplyStacks;
 
 public class Stack
 {
-    private List<char> _crates = new();
+    private List<char> _crates;
     
     public int Id { get; }
 
-    public Stack(int id)
+    public Stack(int id, List<char> crates)
     {
         Id = id;
+        _crates = crates;
     }
-    
-    
+
+    public char GetTopCrate()
+    {
+        return _crates[0];
+    }
+
+    public IEnumerable<char> RemoveCratesFromTop(int quantity)
+    {
+        var cratesToMove = _crates.GetRange(0, quantity);
+        _crates.RemoveRange(0, quantity);
+        return cratesToMove;
+    }
+
+    public void AddCratesOnTop(IEnumerable<char> crates)
+    {
+        var tempCrates = _crates;
+        _crates = new List<char>();
+        _crates.AddRange(crates);
+        _crates.AddRange(tempCrates);
+    }
 }
